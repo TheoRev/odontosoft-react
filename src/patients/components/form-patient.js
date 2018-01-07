35707a62901ac8263686a250484715cb7191726b
TheoRev <hrevillafernandez79@gmail.com>
2018-01-07 17:39:18 -0500

    Creacion de un nuevo paciente usando el API REST de Go

--- src/patients/components/form-patient.js
+++ src/patients/components/form-patient.js
@@ -20,8 +20,7 @@ function FomPatient(props) {
                 isOpen={props.modal}
                 toggle={props.toggle}
                 backdrop={"static"}
-                size={"lg"}
-            >
+                size={"lg"}>
                 <form onSubmit={props.execute}>
                     <ModalHeader toggle={props.toggle}>PACIENTE</ModalHeader>
                     <ModalBody>
@@ -32,76 +31,158 @@ function FomPatient(props) {
                                         <Label for="" size="sm">
                                             ID
                                         </Label>
-                                        <Input type="text" name="txtId" disabled bsSize="sm" />
+                                        <Input
+                                            type="text"
+                                            name="txtId"
+                                            disabled
+                                            bsSize="sm"
+                                            value={props.ID}
+                                            onChange={props.setId}/>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label size="sm">Fecha</Label>
-                                        <Input type="date" name="txtFecha" bsSize="sm" required />
+                                        <Input
+                                            type="date"
+                                            name="txtFecha"
+                                            bsSize="sm"
+                                            value={props.dateInit}
+                                            onChange={props.setDateInit}
+                                            required/>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label size="sm">Nombre y Ape.</Label>
-                                        <Input type="text" name="txtNomApe" bsSize="sm" required />
+                                        <Input
+                                            type="text"
+                                            name="txtNomApe"
+                                            bsSize="sm"
+                                            value={props.nomApe}
+                                            onChange={props.setNomApe}
+                                            required/>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label size="sm">Edad</Label>
+                                        <Input
+                                            type="text"
+                                            name="txtAge"
+                                            bsSize="sm"
+                                            value={props.age}
+                                            onChange={props.setAge}
+                                            required/>
                                     </FormGroup>
                                     <FormGroup check inline>
                                         <Label size="sm">Sexo:</Label>
                                         <FormGroup check>
                                             <Label check>
-                                                <Input type="radio" id="rbMasc" name="rbSex" />
+                                                <Input type="radio" id="rbMasc" name="rbSex" value='M' onChange={props.setSex} required />
                                                 Masculino
                                             </Label>
                                         </FormGroup>
                                         <FormGroup check>
                                             <Label check>
-                                                <Input type="radio" id="rbFem" name="rbSex" />
+                                                <Input type="radio" id="rbFem" name="rbSex" value='F' onChange={props.setSex} required />
                                                 Femenino
                                             </Label>
                                         </FormGroup>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label size="sm">Fec. Nacimiento</Label>
-                                        <Input type="date" name="txtFecNac" bsSize="sm" required />
+                                        <Input
+                                            type="date"
+                                            name="txtFecNac"
+                                            bsSize="sm"
+                                            value={props.dateNac}
+                                            onChange={props.setFecNac}
+                                            required/>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label size="sm">Dirección</Label>
-                                        <Input type="text" name="txtDir" bsSize="sm" required />
-                                    </FormGroup>
-                                    <FormGroup>
-                                        <Label size="sm">Ocupación</Label>
-                                        <Input type="text" name="txtOcup" bsSize="sm" required />
+                                        <Input
+                                            type="text"
+                                            name="txtDir"
+                                            bsSize="sm"
+                                            value={props.address}
+                                            onChange={props.setAddress}
+                                            required/>
                                     </FormGroup>
                                 </div>
                                 <div className="col-sm-6">
                                     <FormGroup>
+                                        <Label size="sm">Ocupación</Label>
+                                        <Input
+                                            type="text"
+                                            name="txtOcup"
+                                            bsSize="sm"
+                                            value={props.ocupation}
+                                            onChange={props.setOcupation}
+                                            required/>
+                                    </FormGroup>
+                                    <FormGroup>
                                         <Label size="sm">Telef. / Cel.</Label>
-                                        <Input type="text" name="txtTelefCel" bsSize="sm" required />
+                                        <Input
+                                            type="text"
+                                            name="txtTelefCel"
+                                            bsSize="sm"
+                                            value={props.telCel}
+                                            onChange={props.setTelefCel}
+                                            required/>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label size="sm">Alergias</Label>
-                                        <Input type="text" name="txtAlergias" bsSize="sm" required />
+                                        <Input
+                                            type="text"
+                                            name="txtAlergias"
+                                            bsSize="sm"
+                                            value={props.alergies}
+                                            onChange={props.setAlergies}
+                                            required/>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label size="sm">Operaciones</Label>
-                                        <Input type="text" name="txtOperaciones" bsSize="sm" required />
+                                        <Input
+                                            type="text"
+                                            name="txtOperaciones"
+                                            bsSize="sm"
+                                            value={props.operations}
+                                            onChange={props.setOperations}
+                                            required/>
                                     </FormGroup>
                                     <FormGroup check>
                                         <Label check>
-                                            <Input type="checkbox" name="cbDiab" />
+                                            <Input
+                                                type="checkbox"
+                                                name="cbDiab"
+                                                checked={props.diabettes}
+                                                onChange={props.setDiabettes}/>
                                             Diabettes
                                         </Label>
                                     </FormGroup>
                                     <FormGroup check>
                                         <Label check>
-                                            <Input type="checkbox" name="cbHiper" />
+                                            <Input
+                                                type="checkbox"
+                                                name="cbHiper"
+                                                checked={props.hipertension}
+                                                onChange={props.setHipertension}/>
                                             Hipertension
                                         </Label>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label size="sm">Otros</Label>
-                                        <Input type="textarea" name="txtOtros" bsSize="sm" />
+                                        <Input
+                                            type="textarea"
+                                            name="txtOtros"
+                                            bsSize="sm"
+                                            value={props.others}
+                                            onChange={props.setOthers}/>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label size="sm">Trat. Médicos</Label>
-                                        <Input type="textarea" name="txtTratMed" bsSize="sm" />
+                                        <Input
+                                            type="textarea"
+                                            name="txtTratMed"
+                                            bsSize="sm"
+                                            value={props.treatMedics}
+                                            onChange={props.setTreatMedics}/>
                                     </FormGroup>
                                 </div>
                             </div>
--- src/patients/containers/patient.js
+++ src/patients/containers/patient.js
@@ -1,16 +1,38 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Patients from "../components/patients";
 import PatientService from "../../services/patient";
 import request from "superagent";
 import FormPatient from "../components/form-patient";
 
 class Patient extends Component {
+  moment = require("moment");
   state = {
     dark: "true",
     height: "300px",
     servPatients: new PatientService(),
     listPatients: [],
-    modal: false
+    modal: false,
+    ID: 0,
+    dateInit: this
+      .moment(new Date())
+      .format("YYYY-MM-DD"),
+    // dateInit:'',
+    nomApe: '',
+    age: '',
+    masc: true,
+    fem: false,
+    sex: '',
+    dateNac: '',
+    address: '',
+    ocupation: '',
+    telCel: '',
+    alergies: '',
+    operations: '',
+    diabettes: false,
+    hipertension: false,
+    others: '',
+    treatMedics: '',
+    state: true
   };
 
   toggle = () => {
@@ -19,30 +41,163 @@ class Patient extends Component {
     });
   };
 
-  execute = (event) => {
-    event.preventDefault();
-    console.log(event.target.value);
+  setId = (event) => {
+    this.setState({ID: event.target.value})
   };
 
+  setDateInit = (event) => {
+    this.setState({dateInit: event.target.value});
+  };
+
+  setNomApe = (event) => {
+    this.setState({nomApe: event.target.value});
+  };
+
+  setAge = (event) => {
+    this.setState({age: event.target.value});
+  };
+
+  setSex = (event) => {
+    // console.log(event.target.value);
+    this.setState({sex: event.target.value});
+  };
+
+  setFecNac = (event) => {
+    this.setState({dateNac: event.target.value});
+  };
+
+  setAddress = (event) => {
+    this.setState({address: event.target.value});
+  };
+
+  setOcupation = (event) => {
+    this.setState({ocupation: event.target.value});
+  };
+
+  setTelefCel = (event) => {
+    this.setState({telCel: event.target.value});
+  };
+  setAlergies = (event) => {
+    this.setState({alergies: event.target.value});
+  };
+  setOperations = (event) => {
+    this.setState({operations: event.target.value});
+  };
+
+  setDiabettes = (event) => {
+    this.setState({diabettes: event.target.checked});
+  };
+
+  setHipertension = (event) => {
+    this.setState({hipertension: event.target.checked});
+  };
+
+  setOthers = (event) => {
+    this.setState({others: event.target.value});
+  };
+
+  setTreatMedics = (event) => {
+    this.setState({treatMedics: event.target.value});
+  };
+
+  execute = (event) => {
+    this.createPatient();
+    event.preventDefault();
+    // console.log(event.target.value);
+  };
+
+  createPatient = () => {
+    console.log("Creating patient...")
+    request
+      .post("localhost:3030/api/crud/patients/")
+      .send({
+        ID: 0,
+        dateInit: this
+          .moment(this.state.dateInit)
+          .format(),
+        nomApe: this.state.nomApe,
+        age: this.state.age,
+        sex: this.state.sex,
+        dateNac: this
+          .moment(this.state)
+          .format(),
+        address: this.state.address,
+        ocupation: this.state.ocupation,
+        telCel: this.state.telCel,
+        alergies: this.state.alergies,
+        operations: this.state.operations,
+        diabettes: this.state.diabettes,
+        hipertension: this.state.hipertension,
+        others: this.state.others,
+        treatMedics: this.state.treatMedics,
+        state: true
+      })
+      .set('Accept', 'application/json')
+      .then((res) => {
+        console.log(res);
+        alert(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   componentDidMount() {
-    request.get("http://localhost:3030/api/patients/").end((err, resp) => {
-      const pacientes = JSON.parse(resp.text);
-      // console.log(pacientes);
-      this.setState({ listPatients: pacientes });
-    });
+    request
+      .get("http://localhost:3030/api/patients/")
+      .end((err, resp) => {
+        const pacientes = JSON.parse(resp.text);
+        // console.log(pacientes);
+        this.setState({listPatients: pacientes});
+      });
   }
 
   render() {
     // console.log("Response:");
-    // console.log(this.state.listPatients);
+    console.log(this.state.listPatients);
+    // console.log(JSON.stringify(this.state.listPatients));
     return (
       <div>
         <Patients
           dark={this.state.dark}
           listPatients={this.state.listPatients}
+          toggle={this.toggle}/>
+        <FormPatient
+          modal={this.state.modal}
           toggle={this.toggle}
-        />
-        <FormPatient modal={this.state.modal} toggle={this.toggle} execute={this.execute} />
+          execute={this.execute}
+          ID={this.state.ID}
+          dateInit={this.state.dateInit}
+          nomApe={this.state.nomApe}
+          age={this.state.age}
+          masc={this.state.masc}
+          fem={this.state.fem}
+          sex={this.state.sex}
+          dateNac={this.state.dateNac}
+          address={this.state.address}
+          ocupation={this.state.ocupation}
+          telCel={this.state.telCel}
+          alergies={this.state.alergies}
+          operations={this.state.operations}
+          diabettes={this.state.diabettes}
+          hipertension={this.state.hipertension}
+          others={this.state.others}
+          treatMedics={this.state.treatMedics}
+          setId={this.setId}
+          setDateInit={this.setDateInit}
+          setNomApe={this.setNomApe}
+          setAge={this.setAge}
+          setSex={this.setSex}
+          setFecNac={this.setFecNac}
+          setAddress={this.setAddress}
+          setOcupation={this.setOcupation}
+          setTelefCel={this.setTelefCel}
+          setAlergies={this.setAlergies}
+          setOperations={this.setOperations}
+          setDiabettes={this.setDiabettes}
+          setHipertension={this.setHipertension}
+          setOthers={this.setOthers}
+          setTreatMedics={this.setTreatMedics}/>
       </div>
     );
   }
